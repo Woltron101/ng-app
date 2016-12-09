41c86712921cfc4348c95aeaf104fdfab43d5205
woltron101 <woltron101@gmail.com>
2016-12-09 10:13:54 +0200

    add popup

--- ctrl/catalogCtrl.js
+++ ctrl/catalogCtrl.js
@@ -1,9 +1,34 @@
- var app = angular.module('app', []);
- app.controller('catalogCtrl', function($scope, $http) {
-     $scope.catalog = 'some string';
-     $http.get('json/catalog.json').success(function(response) {
+var app = angular.module('app', []);
+app.controller('catalogCtrl', function($scope, $http) {
+    $http.get('json/catalog.json').success(function(response) {
+        $scope.catalog = response;
+    });
 
-         $scope.catalog = response;
-     });
+    $scope.popup = {
+        id: 'id1',
+        name: 'phone1',
+        price: 1.99,
+        img: '1',
+        active: false
+    };
 
- })
+    $scope.showPopup = function() {
+        var clickedElId = this.item.id,
+            catalog = $scope.catalog;
+
+        for (var i = catalog.length - 1; i >= 0; i--) {
+            if (catalog[i].id == clickedElId) {
+                $scope.popup = catalog[i]
+                $scope.popup.active = true;
+            }
+        }
+    }
+
+    $scope.hidePopup = function(e) {
+        var target = angular.element(e.target);
+
+        if (target.hasClass('popup-wrap') || target.hasClass('close')) {
+            $scope.popup.active = false;
+        }
+    }
+})
---
+++ ctrl/loginCtrl.js
@@ -0,0 +1,20 @@
+app.controller('loginCtrl', function($scope, $http) {
+    $scope.a = false;
+    var users;
+    $http.get('json/users.json').success(function(response) {
+        users = response;
+    });
+    $scope.logIn = function() {
+        for (var i = users.length - 1; i >= 0; i--) {
+            if ($scope.login == users[i].login && $scope.password == users[i].password) {
+                alert('enter');
+                return false;
+            }
+
+        }
+        alert('error');
+        return false;
+
+    }
+
+})
